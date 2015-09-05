042ef98e1b35bacc71f9c1c90512f2128d6e399d
Matt Hynes <matthynesdev@gmail.com>
2015-09-05 16:47:25 -0230

    Added sidebar animation. About Me section.

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,6 +16,6 @@ $( document ).ready(function() {
 });
 
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/js/particles.json', function() {
+particlesJS.load('particles-js', '../../../../../../assets/js/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
